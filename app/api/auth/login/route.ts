ec0089436e2bd8719d4852189f03902b3aa7c836
Rasim4ik111 <rasim95suleymanli@gmail.com>
2026-05-11 15:44:40 +0400

    feat: add login endpoint with jwt

---
+++ app/api/auth/login/route.ts
@@ -0,0 +1,22 @@
+import { users } from "@/lib/data";
+import { NextResponse } from "next/server";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+export async function POST(request: Request) {
+  const body = await request.json();
+  const { email, password } = body;
+  const user = users.find((user) => user.email === email);
+  if (!user) {
+    return NextResponse.json("User not found", { status: 404 });
+  }
+
+  const match = await bcrypt.compare(password, user?.password);
+  if (!match) {
+    return NextResponse.json("User not found", { status: 404 });
+  }
+
+  const token = jwt.sign({ userId: user.id }, "secret", { expiresIn: "7d" });
+
+  return NextResponse.json({ token });
+}
