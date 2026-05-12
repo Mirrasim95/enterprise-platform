4696ce0c9fd38a88c900ce3455a099937deb9c45
Rasim4ik111 <rasim95suleymanli@gmail.com>
2026-05-12 12:48:06 +0400

    feat: add orders list to dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -5,17 +5,35 @@ import { useQuery } from "@tanstack/react-query";
 export default function Page() {
   const { data, isLoading } = useQuery({
     queryKey: ["products"],
-    queryFn: () => fetch("./api/products").then((res) => res.json()),
+    queryFn: () => fetch("/api/products").then((res) => res.json()),
+  });
+
+  const { data: orders, isLoading: ordersLoading } = useQuery({
+    queryKey: ["orders"],
+    queryFn: () => fetch("/api/orders").then((res) => res.json()),
   });
 
   if (isLoading) {
     return <div>...Loading</div>;
   }
+  if (ordersLoading) {
+    return <div>...Orders is Loading</div>;
+  }
+
   return (
     <div>
       <div>
         {data?.map((item) => (
           <div key={item.id}>{item.name}</div>
+        ))}
+      </div>
+      <div>
+        {orders?.map((item) => (
+          <div key={item.id}>
+            <p>Product Id - {item.productId}</p>
+            <p>Quantity is {item.quantity}</p>
+            <p>Status is {item.status}</p>
+          </div>
         ))}
       </div>
     </div>
